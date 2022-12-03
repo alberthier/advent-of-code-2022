2eea50aa61f43e92f1ffe7bc0a17e1ba8503342a
Eric ALBER <eric.alber@gmail.com>
2022-12-03 23:40:27 +0100

    Add day03 stage2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -16,6 +16,7 @@ pub fn item_value(item: Item) -> u32 {
     }
 }
 
+#[derive(Clone)]
 pub struct Compartment {
     content: String,
 }
@@ -36,6 +37,7 @@ impl Compartment {
     }
 }
 
+#[derive(Clone)]
 pub struct Rucksack {
     compartment: [Compartment; 2],
 }
@@ -60,34 +62,80 @@ impl Rucksack {
     pub fn find_common_item(&self) -> Option<Item> {
         self.compartment[0].find_common_item(&self.compartment[1])
     }
+
+    pub fn iter_all_items(&self) -> impl Iterator<Item = Item> + '_ {
+        self.compartment[0].content.chars().chain(self.compartment[1].content.chars())
+    }
 }
 
-pub struct RucksackGroup {
+pub struct ElfGroup {
     rucksacks: Vec<Rucksack>
 }
 
-impl RucksackGroup {
+impl ElfGroup {
 
-    pub fn from(lines: impl Iterator<Item = String>) -> Option<RucksackGroup> {
+    pub fn new(rucksacks: Vec<Rucksack>) -> ElfGroup {
+        ElfGroup { rucksacks }
+    }
+
+    pub fn find_common_item(&self) -> Option<Item> {
+        for item in self.rucksacks[0].iter_all_items() {
+            let is_common = self.rucksacks.iter().map(|rucksack| {
+                rucksack.iter_all_items()
+                    .find(|other_item| *other_item == item)
+                    .is_some()
+            }).all(|x| x);
+            if is_common { return Some(item) }
+        };
+        None
+    }
+}
+
+pub struct ElvesColony {
+    elf_groups: Vec<ElfGroup>,
+}
+
+impl ElvesColony {
+
+    pub fn from(lines: impl Iterator<Item = String>, group_size: usize) -> Option<ElvesColony> {
+        let mut instance = ElvesColony {
+            elf_groups: Vec::new(),
+        };
         let mut rucksacks = Vec::new();
         for line in lines {
             let Some(rucksack) = Rucksack::from(&line) else { return None };
             rucksacks.push(rucksack)
         }
-        let instance = RucksackGroup { rucksacks };
+
+        for group_rucksacks in rucksacks.chunks(group_size).into_iter() {
+            let elf_group = ElfGroup::new(group_rucksacks.to_owned().to_vec());
+            instance.elf_groups.push(elf_group);
+        }
+        println!("count {}", instance.elf_groups.len());
+
         Some(instance)
     }
 
     pub fn common_items_sum(&self) -> u32 {
         let mut result = 0;
-        for rucksack in self.rucksacks.iter() {
-            if let Some(item) = rucksack.find_common_item() {
-                let item_val = item_value(item);
-                // println!("{item} = {item_val}");
-                result += item_val;
+        for elf_group in self.elf_groups.iter() {
+            for rucksack in elf_group.rucksacks.iter() {
+                if let Some(item) = rucksack.find_common_item() {
+                    let item_val = item_value(item);
+                    // println!("{item} = {item_val}");
+                    result += item_val;
+                }
             }
         }
         result
+    }
+
+    pub fn badges_sum(&self) -> Option<u32> {
+        self.elf_groups.iter().try_fold(0, |acc, elf_group| {
+            let Some(item) = elf_group.find_common_item() else { return None };
+            let value = item_value(item);
+            Some(acc + value)
+        })
     }
 }
 
@@ -97,11 +145,28 @@ fn stage1(path: &str) -> Result<(), &'static str> {
         return Err("Unable to read the file")
     };
 
-    let Some(rucksack_group) = RucksackGroup::from(lines) else {
+    let Some(rucksack_group) = ElvesColony::from(lines, 1) else {
         return Err("Unable to load rucksack")
     };
     let sum = rucksack_group.common_items_sum();
     println!("All common items in rucksacks values sum {sum}");
+
+    Ok(())
+}
+
+
+fn stage2(path: &str) -> Result<(), &'static str> {
+    let Ok(lines) = read_lines(path) else {
+        return Err("Unable to read the file")
+    };
+
+    let Some(rucksack_group) = ElvesColony::from(lines, 3) else {
+        return Err("Unable to load rucksack")
+    };
+    let Some(sum) = rucksack_group.badges_sum() else {
+        return Err("Unable to find a common item in all groups");
+    };
+    println!("All groups badges sum {sum}");
 
     Ok(())
 }
@@ -113,6 +178,7 @@ fn main() -> Result<(), &'static str> {
 
     match stage.as_str() {
         "stage1" => stage1(path.as_str()),
+        "stage2" => stage2(path.as_str()),
         _ => Err("Unknown stage")
     }
 }
