32534bef5dee6615918dab357e3c9bab2f26ab7b
Eric ALBER <eric.alber@gmail.com>
2022-12-04 22:52:38 +0100

    Add day04 stage 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -5,8 +5,21 @@ use std::ops::Range;
 use once_cell::sync::Lazy;
 use regex::Regex;
 
+
 type SectionID = i32;
 type SectionRange = Range<SectionID>;
+
+
+pub fn are_section_ranges_fully_overlapping(lhs: &SectionRange, rhs: &SectionRange) -> bool {
+    lhs.start >= rhs.start && lhs.end <= rhs.end ||
+    rhs.start >= lhs.start && rhs.end <= lhs.end
+}
+
+pub fn are_section_ranges_partially_overlapping(lhs: &SectionRange, rhs: &SectionRange) -> bool {
+    are_section_ranges_fully_overlapping(lhs, rhs) ||
+    rhs.contains(&lhs.start) || rhs.contains(&lhs.end) ||
+    lhs.contains(&rhs.start) || lhs.contains(&rhs.end)
+}
 
 pub struct ElvesPair {
     first: SectionRange,
@@ -32,7 +45,16 @@ impl ElvesPair {
 
         Some(ElvesPair { first, second })
     }
+
+    pub fn is_fully_overlapping(&self) -> bool {
+        are_section_ranges_fully_overlapping(&self.first, &self.second)
+    }
+
+    pub fn is_partially_overlapping(&self) -> bool {
+        are_section_ranges_partially_overlapping(&self.first, &self.second)
+    }
 }
+
 
 struct ElvesColony {
     groups: Vec<ElvesPair>
@@ -48,46 +70,46 @@ impl ElvesColony {
         Some(ElvesColony { groups })
     }
 
-    pub fn fully_overlapping_sections_sum(&self) -> usize {
-        self.groups.iter().filter_map(|p| {
-            let fully_overlapping =
-                p.first.start >= p.second.start && p.first.end <= p.second.end ||
-                p.second.start >= p.first.start && p.second.end <= p.first.end;
-            fully_overlapping.then_some(())
-        }).count()
+    pub fn fully_overlapping_sections_count(&self) -> usize {
+        self.groups
+            .iter()
+            .filter_map(|p| p.is_fully_overlapping().then_some(()))
+            .count()
+    }
+
+    pub fn partially_overlapping_sections_count(&self) -> usize {
+        self.groups
+            .iter()
+            .filter_map(|p| p.is_partially_overlapping().then_some(()))
+            .count()
     }
 }
 
-fn stage1(path: &str) -> Result<(), &'static str> {
+
+fn execute(path: &str, fully_overlapping: bool) -> Result<(), &'static str> {
     let Ok(lines) = read_lines(path) else {
         return Err("Unable to read the file")
     };
 
     let colony = ElvesColony::from(lines).ok_or("Unable to read the input")?;
-    let sum = colony.fully_overlapping_sections_sum();
-
-    println!("The number of groups with fully overlapping sections is {sum}");
-
-    Ok(())
-}
-
-
-fn stage2(path: &str) -> Result<(), &'static str> {
-    let Ok(_lines) = read_lines(path) else {
-        return Err("Unable to read the file")
-    };
+    if fully_overlapping {
+        let count = colony.fully_overlapping_sections_count();
+        println!("The number of groups with fully overlapping sections is {count}");
+    } else {
+        let count = colony.partially_overlapping_sections_count();
+        println!("The number of groups with partially overlapping sections is {count}");
+    }
 
     Ok(())
 }
-
 
 fn main() -> Result<(), &'static str> {
     let stage = std::env::args().nth(1).expect("Expecting puzzle stage");
     let path = std::env::args().nth(2).expect("Expecting a file name");
 
     match stage.as_str() {
-        "stage1" => stage1(path.as_str()),
-        "stage2" => stage2(path.as_str()),
+        "stage1" => execute(path.as_str(), true),
+        "stage2" => execute(path.as_str(), false),
         _ => Err("Unknown stage")
     }
 }
